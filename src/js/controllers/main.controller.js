92835b935af712eb135afea4319e80be92e34fea
Tim Whitacre <tim@timw.co>
2016-07-19 11:58:18 -0400

    finished for today

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,5 +1,15 @@
 function MainController ($scope) {
-  $scope.greeting = 'Hello World';
+
+  $scope.tasks = [];
+
+  $scope.addTodoItem = function (task) {
+
+    let myTask = { name: task, id: $scope.tasks.length + 1 };
+
+    $scope.tasks.push(myTask);
+    $scope.newtask = '';
+  }
+
 }
 
 MainController.$inject = ['$scope'];
